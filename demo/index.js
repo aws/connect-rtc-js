ac3d3be824dcdcc577009cb20129a184bc65e6db
Yuliang Zhou <yuliangz@amazon.com>
2017-05-08 18:39:19 -0700

    Allow user to override audio codec and AEC option.

---
+++ demo/index.js
@@ -0,0 +1,54 @@
+$(document).ready(function () {
+    var audioElement = $('#remoteAudio')[0];
+
+    if (window.location.hash) {
+        $('#softphoneMediaInfo').val(decodeURIComponent(window.location.hash.substr(1)));
+    }
+
+    $('#makeCall').click(function () {
+        var mediaInfo = JSON.parse($('#softphoneMediaInfo').val());
+        var rtcConfig = mediaInfo.webcallConfig;
+        var session = new connect.RTCSession(rtcConfig.signalingEndpoint,
+            rtcConfig.iceServers,
+            mediaInfo.callContextToken,
+            console);
+
+        session.echoCancellation = $('#echoCancellationOption').is(':checked');
+
+        session.remoteAudioElement = audioElement;
+
+        var statsCollector;
+        session.onSessionConnected = () => {
+            statsCollector = setInterval(() => {
+                var collectTime = new Date();
+                Promise.all([session.getUserAudioStats(), session.getRemoteAudioStats()]).then((streamStats) => {
+                    console.log(collectTime, streamStats);
+                });
+            }, 2000);
+        };
+        session.onSessionCompleted = () => {
+            if (statsCollector) {
+                clearInterval(statsCollector);
+                statsCollector = null;
+            }
+        };
+
+        $('#makeCall').prop('disabled', true);
+        $('#echoCancellationOption').prop('disabled', true);
+        $('#disconnectCall').prop('disabled', false);
+        
+        $('#disconnectCall').click(function () {
+            if (session) {
+                try {
+                    session.hangup();
+                } finally {
+                    $('#makeCall').prop('disabled', false);
+                    $('#echoCancellationOption').prop('disabled', false);
+                    $('#disconnectCall').prop('disabled', true);
+                }
+            }
+        });
+
+        session.connect();
+    });
+});
