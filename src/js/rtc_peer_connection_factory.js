27f19b5cd69c12f924d3819c4102d5c18c68ff38
Vikas Garg <vggarg@amazon.com>
2019-10-09 14:44:43 -0700

    address cr comments

--- src/js/rtc_peer_connection_factory.js
+++ src/js/rtc_peer_connection_factory.js
@@ -10,17 +10,19 @@ import {
 
 export default class RtcPeerConnectionFactory {
 
+    //transportHandle must be a callback function which should return a promise which is going to return the iceServers
+    //publishError must be a callback function which will publish the passed error message to client browser
     constructor(logger, wssManager, clientId, transportHandle, publishError) {
         assertTrue(isFunction(transportHandle), 'transportHandle must be a function');
-        var self = this;
-        self._logger = logger;
-        self._clientId = clientId;
-        self._wssManager = wssManager;
-        self._requestIceAccess = transportHandle;
-        self._publishError = publishError;
-        self._browserSupported = self._isBrowserSupported();
-        self._initializeWebSocketEventListeners();
-        self._requestPeerConnection();
+        assertTrue(isFunction(publishError), 'publishError must be a function');
+        this._logger = logger;
+        this._clientId = clientId;
+        this._wssManager = wssManager;
+        this._requestIceAccess = transportHandle;
+        this._publishError = publishError;
+        this._browserSupported = this._isBrowserSupported();
+        this._initializeWebSocketEventListeners();
+        this._requestPeerConnection();
     }
 
     _isBrowserSupported() {
@@ -29,26 +31,24 @@ export default class RtcPeerConnectionFactory {
 
     //This will handle the idleConnection and quota limits notification from the server
     _webSocketManagerOnMessage(event) {
-        var self = this;
         let content;
         if (event.content) {
             content = JSON.parse(event.content);
         }
-        if (content && self._clientId === content.clientId) {
+        if (content && this._clientId === content.clientId) {
             if (content.jsonRpcMsg.method === "idleConnection") {
-                self._clearIdleRtcPeerConnection();
+                this._clearIdleRtcPeerConnection();
             } else if (content.jsonRpcMsg.method === "quotaBreached") {
-                self._logger.log("Number of active sessions are more then allowed limit for the client " + self._clientId);
-                self._closeRTCPeerConnection();
-                self._publishError("multiple_softphone_active_sessions", "Number of active sessions are more then allowed limit.", "");
+                this._logger.log("Number of active sessions are more then allowed limit for the client " + this._clientId);
+                this._closeRTCPeerConnection();
+                this._publishError("multiple_softphone_active_sessions", "Number of active sessions are more then allowed limit.", "");
             }
         }
     }
 
     _initializeWebSocketEventListeners() {
-        var self = this;
-        self._wssManager.subscribeTopics([SOFTPHONE_ROUTE_KEY]);
-        self._unSubscribe = self._wssManager.onMessage(SOFTPHONE_ROUTE_KEY, hitch(self, self._webSocketManagerOnMessage));
+        this._wssManager.subscribeTopics([SOFTPHONE_ROUTE_KEY]);
+        this._unSubscribe = this._wssManager.onMessage(SOFTPHONE_ROUTE_KEY, hitch(this, this._webSocketManagerOnMessage));
     }
 
     // This method will create and return new peer connection if browser is not supporting early ice collection.
@@ -56,17 +56,17 @@ export default class RtcPeerConnectionFactory {
     get(iceServers) {
         var self = this;
         var pc = self._pc;
-        setTimeout(() => {
-            self._pc = null;
-            self._requestPeerConnection();
-        }, 0);
-
+        self._pc = null;
         if (pc == null) {
             pc = self._createRtcPeerConnection(iceServers);
         }
         if (self._idleRtcPeerConnectionTimerId) {
             clearTimeout(self._idleRtcPeerConnectionTimerId);
+            self._idleRtcPeerConnectionTimerId = null;
         }
+        setTimeout(() => {
+            self._requestPeerConnection();
+        }, 0);
         return pc;
     }
 
@@ -75,7 +75,7 @@ export default class RtcPeerConnectionFactory {
         var self = this;
         if (self._browserSupported) {
             self._requestIceAccess().then(function (response) {
-                    self._pc = self._createRtcPeerConnection(response.softphoneTransport.softphoneMediaConnections);
+                    self._pc = self._createRtcPeerConnection(response);
                     self._idleRtcPeerConnectionTimerId = setTimeout(hitch(self, self._clearIdleRtcPeerConnection), RTC_PEER_CONNECTION_IDLE_TIMEOUT_MS);
                 },
                 // eslint-disable-next-line no-unused-vars
@@ -85,23 +85,21 @@ export default class RtcPeerConnectionFactory {
     }
 
     _createRtcPeerConnection(iceServers) {
-        RTC_PEER_CONNECTION_CONFIG.iceServers = iceServers;
-        RTC_PEER_CONNECTION_CONFIG.iceCandidatePoolSize = DEFAULT_ICE_CANDIDATE_POOL_SIZE;
-        return new RTCPeerConnection(RTC_PEER_CONNECTION_CONFIG, RTC_PEER_CONNECTION_OPTIONAL_CONFIG);
+        var rtcPeerConnectionConfig = RTC_PEER_CONNECTION_CONFIG;
+        rtcPeerConnectionConfig.iceServers = iceServers;
+        rtcPeerConnectionConfig.iceCandidatePoolSize = DEFAULT_ICE_CANDIDATE_POOL_SIZE;
+        return new RTCPeerConnection(rtcPeerConnectionConfig, RTC_PEER_CONNECTION_OPTIONAL_CONFIG);
     }
 
     _clearIdleRtcPeerConnection() {
-        var self = this;
-        self._logger.log("session is idle from long time. closing the peer connection for client " + self._clientId);
-        self._isConnectionIdle = true;
-        self._closeRTCPeerConnection();
+        this._logger.log("session is idle from long time. closing the peer connection for client " + this._clientId);
+        this._closeRTCPeerConnection();
     }
 
     _closeRTCPeerConnection() {
-        var self = this;
-        if (self._pc) {
-            self._pc.close();
-            self._pc = null;
+        if (this._pc) {
+            this._pc.close();
+            this._pc = null;
         }
     }
 }
